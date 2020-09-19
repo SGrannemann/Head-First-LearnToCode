6e5de7b44b09e114525610ae6ac5e0352ff7b6b1
Soeren Grannemann <64766572+SGrannemann@users.noreply.github.com>
2020-09-19 17:17:06 +0200

    Completed model of MVC.

--- GameOfLife/model.py
+++ GameOfLife/model.py
@@ -1,25 +1,43 @@
+import random
+
 height = 100
 width = 100
 
+def randomize(grid, width, height):
+    for i in range(0, height):
+        for j in range(0, width):
+            grid[i][j]=random.randint(0,1)
+
 grid_model = [0] * height
+next_grid_model = [0] * height
+
 for i in range(height):
     grid_model[i] = [0] * width
+    next_grid_model[i] = [0] * width
+
+randomize(grid_model, width, height)
+
 
 def next_gen():
-    global grid_model
+    global grid_model, next_grid_model
 
     for i in range(0, height):
         for j in range(0, width):
             cell = 0
-            print('Checking cell', i, j)
+            
             count = count_neighbors(grid_model, i, j)
         
-        if grid_model[i][j] == 0:
-            if count == 3:
-                cell = 1
-        elif grid_model[i][j] == 1:
-            if count == 2 or count == 3:
-                cell = 1
+            if grid_model[i][j] == 0:
+                if count == 3:
+                    cell = 1
+            elif grid_model[i][j] == 1:
+                if count == 2 or count == 3:
+                    cell = 1
+            next_grid_model[i][j] = cell
+            
+    temp = grid_model
+    grid_model = next_grid_model
+    next_grid_model = temp
 
 def count_neighbors(grid, row, col):
     count = 0
